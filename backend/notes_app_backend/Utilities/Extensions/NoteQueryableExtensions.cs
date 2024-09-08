b2b9380fa05d2be116dd9a4973282b7319195b3b
Malnormalulos-git <malnormalulo1@gmail.com>
2024-09-08 18:58:49 +0300

    backend - sort with dynamic linq

--- backend/notes_app_backend/Utilities/Extensions/NoteQueryableExtensions.cs
+++ backend/notes_app_backend/Utilities/Extensions/NoteQueryableExtensions.cs
@@ -1,23 +1,41 @@
-﻿using System.Linq.Expressions;
+﻿using System.Linq.Dynamic.Core;
+using System.Reflection;
 using notes_app_backend.Data;
 
 namespace notes_app_backend.Utilities.Extensions;
 
 public static class NoteQueryableExtensions
 {
+    private static readonly Dictionary<SortType, string> SortTypeToPropertyMap = new();
+
+    static NoteQueryableExtensions()
+    {
+        InitializeSortTypeToPropertyMap();
+    }
+
+    private static void InitializeSortTypeToPropertyMap()
+    {
+        var noteProperties = typeof(Note).GetProperties().ToDictionary(p => p.Name.ToLowerInvariant());
+        
+        foreach (SortType sortType in Enum.GetValues(typeof(SortType)))
+        {
+            var propertyName = sortType.ToString().Substring(2); // Remove "by" prefix
+            if (noteProperties.TryGetValue(propertyName.ToLowerInvariant(), out PropertyInfo property))
+            {
+                SortTypeToPropertyMap[sortType] = property.Name;
+            }
+        }
+    }
+
     public static IQueryable<Note> OrderByNoteSortType(this IQueryable<Note> source, SortType sortType, bool isByDescending)
     {
-        Expression<Func<Note, object>> keySelector = sortType switch
+        if (!SortTypeToPropertyMap.TryGetValue(sortType, out string propertyName))
         {
-            SortType.byTitle => note => note.Title,
-            SortType.byContent => note => note.Content,
-            SortType.byCreationTime => note => note.CreatedAt,
-            SortType.byLastUpdateTime => note => note.LastUpdatedAt,
-            _ => note => note.LastUpdatedAt
-        };
+            propertyName = nameof(Note.LastUpdatedAt); // Default sorting
+        }
 
-        return isByDescending
-            ? source.OrderByDescending(keySelector)
-            : source.OrderBy(keySelector);
+        var orderByDirection = isByDescending ? "descending" : "ascending";
+        
+        return source.OrderBy($"{propertyName} {orderByDirection}");
     }
 }
